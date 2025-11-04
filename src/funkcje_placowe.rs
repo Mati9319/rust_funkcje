a0baf32200ab26b1d1fa1bd4b322a0cf32b451ad
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-05 00:28:35 +0100

    Add files via upload

---
+++ src/funkcje_placowe.rs
@@ -0,0 +1,137 @@
+impl f32 {
+    pub fn round2(&self, _other: &Self) -> f32 {
+        (self * 100.0).round() / 100.0
+    }
+}
+
+pub fn brutto_na_netto(
+    brutto_zus_zdr_pod: f32,        //przychód ozusowany, ozdrowotniony i opodatkowany
+    brutto_zdr_pod: f32,            //przychód ozdrowotniony i opodatkowany
+    brutto_pod: f32,                //przychód opodatkowany
+    brutto_netto: f32,              //przychód bez potrąceń
+    brutto_nie_zus_zdr_pod: f32,    //przychód niewypłacany (ozusowany, ozdrowotniony i opodatkowany)
+    brutto_nie_zdr_pod: f32,        //przychód niewypłacany (ozdrowotniony i opodatkowany)
+    brutto_nie_pod: f32,            //przychód niewypłacany (opodatkowany)
+    brutto_nie_netto: f32,          //przychód niewypłacany (bez potrąceń)
+    potr_dod: f32,                  //dodatkowe potrącenia
+    pod_zwol: bool,                 //czy zwolnienie z podatku
+    jakie_kup: char,                //'0' <- brak kosztów, '1' <- 250, '2' <- 300
+    jaki_pod_proc: char,            //'1' <- 12%, '2' <- 32%
+    jaka_ulga: char                 //'0' <- brak ulgi, '1' <- 300, '2' <- 150, '3' <- 100
+
+) -> Wynik {
+    let kup = match jakie_kup {
+        '1' => 250.0,
+        '2' => 300.0,
+        _ => 0.0
+    };
+
+    let pod_proc = match jaki_pod_proc {
+        '2' => 0.32,
+        _ => 0.12
+    };
+
+    let ulga = match jaka_ulga {
+        '1' => 300.0,
+        '2' => 150.0,
+        '3' => 100.0,
+        _ => 0.0
+    };
+
+    let brutto_cal =    //cały przychód
+        zaokr(brutto_zus_zdr_pod
+        + brutto_zdr_pod
+        + brutto_pod
+        + brutto_netto
+        + brutto_nie_zus_zdr_pod
+        + brutto_nie_zdr_pod
+        + brutto_nie_pod
+        + brutto_nie_netto).round2();
+
+    let brutto_wyp =    //cały wypłacany przychód
+        zaokr(brutto_zus_zdr_pod
+        + brutto_zdr_pod
+        + brutto_pod
+        + brutto_netto).round2();
+
+    let pd_zus =        //przychód stanowiący podstawę składek ZUS
+        zaokr(brutto_zus_zdr_pod
+        + brutto_nie_zus_zdr_pod).round2();
+
+    let pd_zdr =        //przychód stanowiący podstawę składki zdrowotnej
+        zaokr(brutto_zus_zdr_pod
+        + brutto_nie_zus_zdr_pod
+        + brutto_nie_zdr_pod).round2();
+
+    let pd_pod =        //przychód stanowiący podstawę podatku
+        zaokr(brutto_zus_zdr_pod
+        + brutto_zdr_pod
+        + brutto_pod
+        + brutto_nie_zus_zdr_pod
+        + brutto_nie_zdr_pod
+        + brutto_nie_pod).round2();
+
+    let zus_emerytalna = (pd_zus * 0.0976).round2();
+    let zus_rentowa = (pd_zus * 0.015).round2();
+    let zus_chorobowa = (pd_zus * 0.0245).round2();
+
+    let zus = (zus_emerytalna + zus_rentowa + zus_chorobowa).round2(); //suma składek ZUS
+
+    let zdrowotna = ((pd_zdr - zus) * 0.09).round2();
+
+    let podatek = if pod_zwol {
+        0.0
+    } else {
+        ((pd_pod - zus - kup).round() * pod_proc - ulga).round()
+    };
+
+    let netto = (brutto_wyp - zus - zdrowotna - podatek - potr_dod).round2();
+
+    Wynik {
+        brutto_cal,
+        brutto_wyp,
+        zus_emerytalna,
+        zus_rentowa,
+        zus_chorobowa,
+        zdrowotna,
+        podatek,
+        netto
+    }
+}
+
+pub struct Wynik {
+    brutto_cal: f32,
+    brutto_wyp: f32,
+    zus_emerytalna: f32,
+    zus_rentowa: f32,
+    zus_chorobowa: f32,
+    zdrowotna: f32,
+    podatek: f32,
+    netto: f32
+}
+
+impl Wynik {
+    pub fn show(&self) {
+        println!(
+"Całkowite wynagrodzenie brutto: {:.2} zł
+Wypłacane wynagrodzenie brutto: {:.2} zł
+
+Składka na ubezpieczenie emerytalne: {:.2} zł
+Składka na ubezpieczenie rentowe: {:.2} zł
+Składka na ubezpieczenie chorobowe: {:.2} zł
+
+Składka na ubezpieczenie zdrowotne: {:.2} zł
+
+Zaliczka na podatek dochodowy: {:.2} zł
+
+Wypłacane wynagrodzenie netto: {:.2} zł",
+        self.brutto_cal,
+        self.brutto_wyp,
+        self.zus_emerytalna,
+        self.zus_rentowa,
+        self.zus_chorobowa,
+        self.zdrowotna,
+        self.podatek,
+        self.netto);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod funkcje_placowe;
+use funkcje_placowe::*;
+
+fn main() {
+    let x = brutto_na_netto(6500.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, false, '2', '1', '1');
+    x.show();
+}
