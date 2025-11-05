cbba240b5f9efa31743d7632067a2d20b1253a1b
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-05 21:51:17 +0100

    Remove redundant method call on x
    Removed unnecessary call to 'x.wiecej(3.0)' and its subsequent display.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,4 @@ use funkcje_placowe::*;
 fn main() {
     let mut x = brutto_na_netto(2644.58, 2105.76, 1691.4, 72.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
     x.show();
-    x.wiecej(3.0);
-    x.show();
 }
