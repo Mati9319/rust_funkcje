d72d675e30f2df73b470ccb58ba414ec58c6937c
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-06 00:32:28 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod funkcje_placowe;
 use funkcje_placowe::*;
 
 fn main() {
-    let x = Lista_plac::nalicz(2644.58, 2105.76, 1691.4, 72.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
+    let x = ListaPlac::nalicz(2644.58, 2105.76, 1691.4, 72.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
     x.show();
 }
