48bee9e40c68a1d92bb100033580d1acc427bb16
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-05 21:47:32 +0100

    Change x to mutable for further modifications

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod funkcje_placowe;
 use funkcje_placowe::*;
 
 fn main() {
-    let x = brutto_na_netto(2644.58, 2105.76, 1691.4, 72.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
+    let mut x = brutto_na_netto(2644.58, 2105.76, 1691.4, 72.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
     x.show();
     x.wiecej(3.0);
     x.show();
