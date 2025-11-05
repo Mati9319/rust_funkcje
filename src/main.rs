9a64f6c9cf34ca66f1d8c061d6af390c81f9dcc5
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-05 21:46:33 +0100

    Change argument type for wiecej method call

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use funkcje_placowe::*;
 fn main() {
     let x = brutto_na_netto(2644.58, 2105.76, 1691.4, 72.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
     x.show();
-    x.wiecej(3);
+    x.wiecej(3.0);
     x.show();
 }
