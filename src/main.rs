3e605f19107dc6f90b4f39e936ac7326138ccf85
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-05 09:29:51 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod funkcje_placowe;
 use funkcje_placowe::*;
 
 fn main() {
-    let x = brutto_na_netto(2666.58, 2105.76, 1691.4, 50.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
+    let x = brutto_na_netto(2594.58, 2105.76, 1691.4, 122.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
     x.show();
 }
