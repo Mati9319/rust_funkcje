ada751b0dbab86baab0b39455fd311a331f605ba
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-06 01:11:53 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     let mut x = ListaPlac::nalicz(2644.58, 2105.76, 1691.4, 72.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
     x.show();
     x.pod_zwol = true;
+    x.brutto_zus_zdr_pod += 1000.00;
     x.przelicz();
     x.show();
 }
