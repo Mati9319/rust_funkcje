4cd8cca589627ac759196c68ecf6187dc8ae96fb
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-06 10:36:51 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crate::lista_plac::*;
+use testowanie::lista_plac::*;
 
 fn main() {
     let mut x = ListaPlac::nalicz(2644.58, 2105.76, 1691.4, 72.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
