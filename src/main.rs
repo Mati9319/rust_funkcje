0ac7c997ba9b3d06cc81ed696caacf6c9f5c41a7
Mateusz Rzempołuch <mateusz_rzempoluch@icloud.com>
2025-11-12 12:08:17 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use testowanie::lista_plac::*;
 
 fn main() {
-    let mut x = ListaPlac::nalicz(2644.58, 2105.76, 1691.4, 72.0, 100.0, 0.0, 0.0, 0.0, 550.0, false, '2', '1', '1');
+    let mut x = ListaPlac::nalicz(5300.0+262.96+52.08+863.52+78.4+50.0+31.92+64.12+46.2+9.8+92.68+689.0+9.8+164.0+1457.0+249.76+795.0+1754.64+274.68+225.22+44.8, 0.0, 0.0, 625.0+441.0, 100.0, 0.0, 0.0, 0.0, 20.2+500.0+300.0+53.0, false, '2', '1', '1');
     x.wyswietl();
+    x.jaka_ulga = '2';
+    x.przelicz_i_wyswietl();
     x.pod_zwol = true;
-    x.brutto_zus_zdr_pod += 1000.00;
-    x.przelicz();
-    x.wyswietl();
+    x.przelicz_i_wyswietl();
 }
